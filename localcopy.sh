d1b97aa35ce3ea825a7bd9fbcd11aa0332205070
shrey-jain <jainshreydel@gmail.com>
2020-08-21 15:38:55 +0530

    week2 assingment

---
+++ localcopy.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile="C:\Users\SONIA\git_projects\pjp2.0\Mscripts\target\Mscripts-1.0-SNAPSHOT.jar" -DgroupId="com.ps.app" -DartifactId="Mscript" -Dversion="1.0-SNAPSHOT" -Dpackaging="jar"
---
+++ logbuild.sh
@@ -0,0 +1,1 @@
+mvn -l log.txt
